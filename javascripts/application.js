7c5292c4b09e214091317a06606c0e97db9c2660
Daniel Manzke <daniel.manzke@googlemail.com>
2012-10-23 17:01:32 +0200

    added submit

--- javascripts/application.js
+++ javascripts/application.js
@@ -45,3 +45,55 @@ function getFileContentTheNewWay2(){
         });
     });
 }
+
+function submitPres() {
+    try {
+
+        // Try to get the entire PowerPoint presentation.
+        Office.context.document.getFileAsync("compressed", function(result) {
+
+            // Check to see whether the attempt to get the file was successful.
+            if (result.status == "succeeded") {
+
+                // Get the File object from the result.
+                var myFile = result.value;
+                statusInfo.innerHTML = "Getting file of " + myFile.size + 
+                    " bytes<br/>";
+
+                // Iterate over each slice in the file and access each chunk.
+                for (var i = 0; i < myFile.sliceCount; i++) {
+
+                    myFile.getSliceAsync(i, function(result) {
+
+                        // If the call returns successfully, we have access
+                        // to the Slice object.
+                        if (result.status == "succeeded") {
+
+                           // Send the slice to the web service.
+                           statusInfo.innerHTML += "Sending piece " + i + 
+                               " of " + myFile.sliceCount + "<br/>";
+                           sendSlice(result.value);
+                        }
+                    });
+                }
+
+                // Close the file when we’re done with it.
+                myFile.closeAsync(function (result) {
+
+                    // If the result returns as a success, the
+                    // file has been successfully closed.
+                    if (result.status == "succeeded") {
+                        statusInfo.innerHTML += "File closed.<br/>";
+                    }
+                    else {
+                        statusInfo.innerHTML += 
+                            "File couldn't be closed.<br/>";
+                    }
+                });
+            }
+        });
+    }
+    catch (err) {
+        statusInfo.innerHTML += (err.name, err.message);
+    }
+}
